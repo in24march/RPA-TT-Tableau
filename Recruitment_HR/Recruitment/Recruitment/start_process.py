d411d0d288b3d59f6c2bac5740714acdf5437105
Nong Joh <chayakorn3065@gmail.com>
2024-10-25 17:07:38 +0700

    delete time

--- Recruitment_HR/Recruitment/Recruitment/start_process.py
+++ Recruitment_HR/Recruitment/Recruitment/start_process.py
@@ -4,7 +4,6 @@ from Master_HRCC import *
 import time
 
 if __name__ == '__main__':
-    # date_get = get_data_web(datetime.now() - timedelta(days=1))
     date_get = datetime.now() - timedelta(days=1)
     time.sleep(1)
     run_map(date_get)   
